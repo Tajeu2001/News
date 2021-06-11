b961a28fae214b683c093196eb56546b03e895ed
Tajeu2001 <tajeusanta@gmail.com>
2021-06-11 11:19:08 +0300

    Eport the news api key

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-# export NEWS_API_KEY = '884fc97d6dcf4391af173d4595dff0ff'
-# export SECRET_KEY='Your secret key'
+export NEWS_API_KEY = '884fc97d6dcf4391af173d4595dff0ff'
+export SECRET_KEY='Your secret key'
 
-# python3 manage.py server
+python3 manage.py server
