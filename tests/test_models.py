3c07ef712e57de8cb07ded615777f594081775fc
Tajeu2001 <tajeusanta@gmail.com>
2021-06-11 10:58:35 +0300

    Create test folder

---
+++ tests/test_models.py
@@ -0,0 +1,18 @@
+# import unittest
+# from app.models import Source,Article
+
+# class TestSource (unittest.TestCase):
+#   def setUp(self):
+#     self.new_source = Source('abc-news' , 'ABC News' , 'Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.', 'general')
+
+#   def test_init(self):
+#     self.assertTrue(isinstance(self.new_source , Source))
+
+
+# class TestArticle(unittest.TestCase):
+#   def setUp(self):
+#     self.new_article = Article('Macdonald' , 'Drugs' , 'Drug addiction being a major challenge the youth are facing', '20th May 2020')
+
+#   def test_init(self):
+#     self.assertTrue(isinstance(self.new_article , Article))
+
