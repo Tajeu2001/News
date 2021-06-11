8390eec5ecfcbf7453855dbd9e9763118b176270
Tajeu2001 <tajeusanta@gmail.com>
2021-06-11 11:27:56 +0300

    Create three classes

--- config.py
+++ config.py
@@ -11,25 +11,25 @@ class Config:
 
 
 
-# class ProdConfig(Config):
-#     '''
-#     Production  configuration child class
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
 
-#     Args:
-#         Config: The parent configuration class with General configuration settings
-#     '''
-#     pass
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
 
 
-# class DevConfig(Config):
-#     '''
-#     Development  configuration child class
+class DevConfig(Config):
+    '''
+    Development  configuration child class
 
-#     Args:
-#         Config: The parent configuration class with General configuration settings
-#     '''
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
 
-#     DEBUG = True
+    DEBUG = True
 
 
 # config_options = {
