a1a093f682a1cfbb90a6cf3d88ea48414a300f5a
Tajeu2001 <tajeusanta@gmail.com>
2021-06-11 11:38:53 +0300

    Import blueprint in the views file

--- app/main/views.py
+++ app/main/views.py
@@ -1,17 +1,17 @@
 from flask import Flask
-# from . import main
-# from ..request import get_sources , get_articles
+from . import main
+from ..request import get_sources , get_articles
 
 
-# @main.route('/')
-# def index():
-#     '''
-#     View root page function that returns the index page and its data
-#     '''
-#     sources = get_sources()
-#     return render_template('index.html',sources=sources)
+@main.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    sources = get_sources()
+    return render_template('index.html',sources=sources)
 
-# @main.route('/source/<source_id>')
+@main.route('/source/<source_id>')
 # def source(source_id):
 #     '''
 #     View source page function that returns a source page 
