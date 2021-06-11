b31ab2ddcdd8c6e0d346fd1a5783093d087b90d1
Tajeu2001 <tajeusanta@gmail.com>
2021-06-11 11:59:02 +0300

    Create a function to process the sources

--- app/request.py
+++ app/request.py
@@ -29,69 +29,68 @@ def get_sources():
 
     return sources_results
 
-# def process_sources(source_list):
+def process_sources(source_list):
+    '''
+    Function  that processes the source results and transform them to a list of objects
+    '''
+
+    source_results = []
+
+    for source_item in source_list:
+        id = source_item.get('id')
+        name = source_item.get('name')
+        description = source_item.get('description')
+        category = source_item.get('category')
+
+        source_object = Source(id,name,description,category)
+
+        source_results.append(source_object)
+
+    return source_results
+
+def get_articles(source):
 #     '''
-#     Function  that processes the source results and transform them to a list of objects
-#     '''
+# #     Function to get  articles
+# #     '''
+# #     get_articles_url = article_url.format(source,api_key)
 
-#     source_results = []
+# #     with urllib.request.urlopen(get_articles_url) as url:
+# #         get_articles_data = url.read()
+# #         get_articles_response = json.loads(get_articles_data)
 
-#     for source_item in source_list:
-#         id = source_item.get('id')
-#         name = source_item.get('name')
-#         description = source_item.get('description')
-#         category = source_item.get('category')
+# #         articles_results = None
 
-#         source_object = Source(id,name,description,category)
+# #         if get_articles_response['articles']:
+# #             articles_results_list = get_articles_response['articles']
+# #             articles_results = process_articles(articles_results_list)
 
-#         source_results.append(source_object)
+# #     return articles_results
 
-#     return source_results
+# # def process_articles(article_list):
+# #     '''
+# #     Function that processes the article results and transform them to a list of objects
+# #     ''' 
 
+# #     article_results = []
 
-# def get_articles(source):
-#     '''
-#     Function to get  articles
-#     '''
-#     get_articles_url = article_url.format(source,api_key)
+# #     for article_item in article_list:
+# #         author = article_item.get('author')
+# #         title = article_item.get('title')
+# #         urlToImage = article_item.get('urlToImage')
+# #         description = article_item.get('description')
+# #         publishedAt = article_item.get('publishedAt')
 
-#     with urllib.request.urlopen(get_articles_url) as url:
-#         get_articles_data = url.read()
-#         get_articles_response = json.loads(get_articles_data)
+# #         if publishedAt != None:
 
-#         articles_results = None
-
-#         if get_articles_response['articles']:
-#             articles_results_list = get_articles_response['articles']
-#             articles_results = process_articles(articles_results_list)
-
-#     return articles_results
-
-# def process_articles(article_list):
-#     '''
-#     Function that processes the article results and transform them to a list of objects
-#     ''' 
-
-#     article_results = []
-
-#     for article_item in article_list:
-#         author = article_item.get('author')
-#         title = article_item.get('title')
-#         urlToImage = article_item.get('urlToImage')
-#         description = article_item.get('description')
-#         publishedAt = article_item.get('publishedAt')
-
-#         if publishedAt != None:
-
-#             # Call publish_date_format method to convert date to a display-friendly format
-#             date_to_display = Article.publish_date_format(publishedAt)
+# #             # Call publish_date_format method to convert date to a display-friendly format
+# #             date_to_display = Article.publish_date_format(publishedAt)
 
 
-#             article_object = Article(author,title,urlToImage,description,date_to_display)
+# #             article_object = Article(author,title,urlToImage,description,date_to_display)
 
-#             article_results.append(article_object)
+# #             article_results.append(article_object)
 
-    return article_results
+#     return article_results
 
 
 
